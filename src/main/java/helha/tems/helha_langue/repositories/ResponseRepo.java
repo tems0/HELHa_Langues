e311160f79ba99ac7b6904b871da60c9c671a3eb
Taj Temsamani <Tetbmen@gmail.com>
2023-08-02 22:54:08 +0200

    ajout du front

--- src/main/java/helha/tems/helha_langue/repositories/ResponseRepo.java
+++
@@ -1,9 +0,0 @@
-package helha.tems.helha_langue.repositories;
-
-import helha.tems.helha_langue.models.Response;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResponseRepo extends JpaRepository<Response,Integer> {
-}
--- src/main/java/helha/tems/helha_langue/repositories/SequenceRepo.java
+++
@@ -1,9 +0,0 @@
-package helha.tems.helha_langue.repositories;
-
-import helha.tems.helha_langue.models.Sequence;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SequenceRepo extends JpaRepository<Sequence,Integer> {
-}
