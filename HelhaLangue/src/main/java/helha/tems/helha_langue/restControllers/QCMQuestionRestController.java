c81eb0704ac7419158f2639ab12f131805d78914
Taj Temsamani <Tetbmen@gmail.com>
2023-08-03 17:00:45 +0200

    changing name

---
+++ HelhaLangue/src/main/java/helha/tems/helha_langue/restControllers/QCMQuestionRestController.java
@@ -0,0 +1,89 @@
+package helha.tems.helha_langue.restControllers;
+
+import helha.tems.helha_langue.models.QCMQuestion;
+import helha.tems.helha_langue.models.Response;
+import helha.tems.helha_langue.services.IQCMQuestionService;
+import helha.tems.helha_langue.services.IResponseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:4200/", allowedHeaders = "*")
+@RestController
+@RequestMapping("/api/qcmQuestions")
+@RequiredArgsConstructor
+public class QCMQuestionRestController {
+
+    @Autowired
+    IQCMQuestionService qcmService;
+
+    @GetMapping("/AllQcms")
+    public  ResponseEntity<List<QCMQuestion>> getAll(){
+        try {
+            final List<QCMQuestion> seqs = qcmService.findAll();
+            if (seqs != null || !seqs.isEmpty()) {
+                return ResponseEntity.status(200).body(seqs);
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(404).body(null);
+        }
+        return  ResponseEntity.status(404).body(null);
+    }
+
+    @GetMapping("/Qcm")
+    public ResponseEntity<QCMQuestion> get(int id){
+
+        try {
+            final QCMQuestion seq = qcmService.findById(id);
+            if (seq != null) {
+                return ResponseEntity.status(200).body(seq);
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(404).body(null);
+        }
+        return  ResponseEntity.status(404).body(null);
+    }
+    @GetMapping("/Qcm/Response")
+    public ResponseEntity<List<Response>> getAllResponse(int id){
+
+        try {
+            final List<Response> reps = qcmService.findAllResponse(id);
+            if (!reps.isEmpty()) {
+                return ResponseEntity.status(200).body(reps);
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(404).body(null);
+        }
+        return  ResponseEntity.status(404).body(null);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<QCMQuestion>> updateQcm(@PathVariable int id, @RequestBody QCMQuestion qcmQuestion)
+    {
+        try {
+            final QCMQuestion qcm = qcmService.findById(id);
+            if (qcm != null) {
+                return ResponseEntity.status(204).body(Optional.ofNullable(qcmService.update(id, qcmQuestion)));
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(404).body(null);
+        }
+        return  ResponseEntity.status(404).body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteQcm(@PathVariable int id)
+    {
+        QCMQuestion qcm = qcmService.findById(id);
+
+        if (qcm == null) {
+            return ResponseEntity.notFound().build();
+        }
+        qcmService.delete(qcm);
+        return ResponseEntity.ok("qcm deleted successfully.");
+    }
+}
---
+++ HelhaLangue/src/test/java/helha/tems/helha_langue/HelhaLangueApplicationTests.java
@@ -0,0 +1,13 @@
+package helha.tems.helha_langue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelhaLangueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
